23057de382bca4d91263d4087ddc4bf398aae35d
christianalcantara <christian.douglas.alcantara@gmail.com>
2021-02-24 02:22:43 -0300

    Github workflow CI

---
+++ config/settings_ci.py
@@ -0,0 +1,9 @@
+"""use sqlite3 database to run tests"""
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
